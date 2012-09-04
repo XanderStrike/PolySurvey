3d32fd7b929e705f33b552697590c9bf6d1e8627
XanderStrike <xanderstrike@gmail.com>
2012-09-04 10:32:54 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolySurvey::Application.config.secret_token = 'a31399ef953529d7d4072b1de683317182364da0fde52b861df7d6c49642281f74b254c1ef9f75bf30143edd84ababe02c4009910a44e5821cc35e8c2cf817e1'
