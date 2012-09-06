f16f07424cbd1fac05b5a24c3aad4ad2dcdb47a4
Wayne Iba <wayne.iba@gmail.com>
2012-09-06 08:59:17 -0700

    migrated to latest db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906052130) do
+ActiveRecord::Schema.define(:version => 20120906061301) do
 
   create_table "results", :force => true do |t|
     t.datetime "created_at",     :null => false
@@ -74,6 +74,10 @@ ActiveRecord::Schema.define(:version => 20120906052130) do
     t.datetime "finish_time"
     t.integer  "match_scenario"
     t.integer  "poll_scenario"
+    t.integer  "vote"
+    t.integer  "time1"
+    t.integer  "time2"
+    t.integer  "time3"
   end
 
   create_table "users", :force => true do |t|
