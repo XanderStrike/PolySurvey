7759b0807119f3cf9d00eded4a67ab5aee9901c5
XanderStrike <xanderstrike@gmail.com>
2012-09-05 20:25:12 -0700

    Changed the way results are pushed

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -19,6 +19,8 @@ class SurveyController < ApplicationController
     else
     	@output = "moderate"
     end
+
+    # Randomly generate three names
     first_names = %w[James John Robert Michael William David Richard Charles Joseph Thomas Christopher Daniel Paul Mark Donald George Kenneth Steven Edward Brian Ronald Anthony Kevin Jason Jeff]
     last_names = %w[Smith Johnson Williams Jones Brown Davis Miller Wilson Moore Taylor Anderson Thomas Jackson White Harris Martin Thompson Robinson Clark Lewis Lee Walker Hall Allen Young King]
 
@@ -26,11 +28,13 @@ class SurveyController < ApplicationController
     @name1 = ["#{first_names[rand(first_names.size)]} #{last_names[rand(last_names.size)]}"]
     @name2 = ["#{first_names[rand(first_names.size)]} #{last_names[rand(last_names.size)]}"]
 
-    case rand(2)
+    # Determine in which way the candidates are displayed (assign them to groups 1-6)
+    group = rand(6)
+    case group
     when 0
       @name0 += ["90%"]
       @name1 += ["40%"]
-      @name2 += ["13%"]
+      @name2 += ["10%"]
     when 1
       if result > 0
         @name0 = ["#{@name0[0]} (Libertarian)", "90%"]
@@ -41,10 +45,37 @@ class SurveyController < ApplicationController
         @name1 = ["#{@name1[0]} (Democrat)", "40%"]
         @name2 = ["#{@name2[0]} (Republican)", "10%"]
       end
+    when 2
+      @name0 += ["90%"]
+      @name1 += ["80%"]
+      @name2 += ["30%"]
+    when 3
+      @name0 += ["90%"]
+      @name1 += ["40%"]
+      @name2 += ["30%"]
+    when 4
+      @name0 += ["90%"]
+      @name1 += ["70%"]
+      @name2 += ["65%"]
+    when 5
+      @name0 += ["90%"]
+      @name1 += ["70%"]
+      @name2 += ["10%"]
     end
 
-    @data = results.join(',') + ",#{result}"
+    @data = results.join(',') + ",#{result},#{@name0.join(',')},#{@name1.join(',')},#{@name2.join(',')},#{group}"
 
   end
 
+  def blurb
+    @data = params["results"].split(',')
+  end
+
+
 end
+
+# The form of "data"
+#   0-14 - Political alignment questions
+#   15 - Political alignment
+#   16, 18, 20 - Candidate names, most to least
+#   17, 19, 21 - Percentages
