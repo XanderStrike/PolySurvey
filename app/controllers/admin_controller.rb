cf34ec8bb7aafafd6986b36335182ffccea6d35e
dray <dray@westmont.edu>
2012-09-13 03:20:32 -0700

    working on csv download

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,20 +8,20 @@ class AdminController < ApplicationController
   def output_results
     results = Results.all
 
-    CSV.open("political_survey_results.csv", "w") do |csv|
+    CSV.open("public/political_survey_results.csv", "w") do |csv|
     unless results.empty?    
         # header row
         csv << results.first.attributes.keys
         # data row
         results.each do |result|
-          puts "result.attributes.values.inspect: #{result.attributes.values.inspect}"
+#          puts "result.attributes.values.inspect: #{result.attributes.values.inspect}"
           csv << result.attributes.values
         end
       end
     end
 
-#    send_file "political_survey_results.csv", :type => "application/csv", :x_sendfile => true
+    send_file "public/political_survey_results.csv", :type => "text/csv", :x_sendfile => true
 
-    render show
+#    render show
   end
 end
