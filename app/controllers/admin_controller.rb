1e3c4ddc5d0588f73207825cc8843c1d6ad5110b
Kelly Macdonald <kmacdonald@westmont.edu>
2012-09-12 22:39:03 -0700

    alignment still fixed

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,25 @@
+class AdminController < ApplicationController
+  def new
+  end
+
+  def panel
+    unless @current_user && @current_user.admin?
+      redirect_to 'root'
+    end
+  end
+
+  def output_results
+    results = Result.all
+
+    user_csv = FasterCSV.generate do |csv|
+      # header row
+      csv << results.first.columns.map(&:name)
+      # data row
+      results.each do |result|
+        csv << result.columns.map(&:value)
+      end
+    end
+
+    send_data(user_csv, :type => 'test/csv', :filename => 'user_record.csv')
+  end
+end
